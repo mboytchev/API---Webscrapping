dbf3e348d1796e3e4a1d96566a9250f901f61e5b
mboytchev <159889061+mboytchev@users.noreply.github.com>
2024-11-26 14:27:23 +0100

    TP2 question

--- TP2 and  3/services/epf-flower-data-science/main.py
+++ TP2 and  3/services/epf-flower-data-science/main.py
@@ -5,4 +5,4 @@ from src.app import get_application
 app = get_application()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", debug=True, reload=True, port=8080)
+    uvicorn.run("main:app", reload=True, port=8080)
